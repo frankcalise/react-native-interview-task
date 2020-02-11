83abef3d55dd81f67bd07033b4838a96d13a43d6
Frank Calise <fcalise@gmail.com>
2020-02-10 22:06:11 -0500

    Animated add icon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Animated, Easing } from "react-native";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import {
   HeaderButtons,
   HeaderButton,
   Item
 } from "react-navigation-header-buttons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const MaterialIconHeaderButton = props => (
   <HeaderButton
@@ -17,6 +18,41 @@ const MaterialIconHeaderButton = props => (
 );
 
 export default function HomeScreen({ navigation }) {
+  const [toggled, setToggled] = React.useState(false)
+  const [animatedValue, setAnimatedValue] = React.useState(
+    new Animated.Value(0)
+  );
+
+  React.useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: toggled ? 0 : 1,
+      duration: 100,
+      useNativeDriver: true,
+      easing: Easing.linear
+    }).start();
+  }, [toggled])
+
+  function rotate() {
+    console.log("rotate!");
+    // if (toggled) {
+    //   // reset
+    //   Animated.timing(animatedValue, {
+    //     toValue: 0,
+    //     duration: 250
+    //   }).start();
+    // } else {
+    //   Animated.timing(animatedValue, {
+    //     toValue: 100,
+    //     duration: 250
+    //   }).start();
+    // }
+    setToggled(!toggled)
+  }
+
+  const rotationAnimation = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "45deg"],
+  });
   return (
     <View style={styles.container}>
       <Text>Amazing, isn't it?</Text>
@@ -26,13 +62,18 @@ export default function HomeScreen({ navigation }) {
           navigation.navigate("Form");
         }}
       />
+      <TouchableOpacity onPress={rotate}>
+        <Animated.View style={{ transform: [{ rotate: rotationAnimation }] }}>
+          <MaterialIcon name="add" size={25} color="black" style />
+        </Animated.View>
+      </TouchableOpacity>
     </View>
   );
 }
 
 HomeScreen.navigationOptions = ({ navigation }) => {
   return {
-    title: 'is this working',
+    title: "is this working",
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={MaterialIconHeaderButton}>
         <Item title="Add" iconName="add" onPress={() => console.log("test")} />
